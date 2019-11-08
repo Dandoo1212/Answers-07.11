a27c036cbef46dbc710c7d46abae6e9993b55be2
dandoo1212 <michalak.damian.kul@gmail.com>
2019-11-08 18:23:16 +0100

    Add implementation of exercises from workshop

---
+++ src/Rozwiazania.java
@@ -0,0 +1,119 @@
+public class Rozwiazania {
+
+
+    public static void main(String[] args) {
+//        ZADANIE 1
+//        System.out.println("Hello KNI!");
+
+//        ZADANIE 2
+//        int number = 5;
+//        System.out.println(number);
+
+
+//        ZADANIE 3
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Give me number: ");
+//        int number;
+//        number = scanner.nextInt();
+//        System.out.println(number);
+
+
+//        ZADANIE 4
+//        Scanner scanner = new Scanner(System.in);
+//        int first ;
+//        int second ;
+//        int third;
+//        System.out.print("Give me first number: ");
+//        first = scanner.nextInt();
+//        System.out.print("Give me second number: ");
+//        second = scanner.nextInt();
+//        System.out.print("Give me third number: ");
+//        third = scanner.nextInt();
+//        System.out.println("" + first + second + third);
+
+//        ZADANIE 5
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Give me day of birth: ");
+//        int dayOfBirth = scanner.nextInt();
+//        System.out.print("Give me month of birth: ");
+//        int monthOfBirth = scanner.nextInt();
+//        LocalDate today = LocalDate.now();
+//        if (today.getDayOfMonth() == dayOfBirth && today.getMonth().getValue() == monthOfBirth) {
+//            System.out.println("Happy Birthday!");
+//        } else {
+//            System.out.println("Sorry, it's not your birthday");
+//        }
+//        ZADANIE 6
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Give me number: ");
+//        int number = scanner.nextInt();
+//        System.out.print("Your month has: ");
+//        switch(number){
+//            case 1:
+//                System.out.print(31);
+//                break;
+//            case 2:
+//                System.out.print(28);
+//                break;
+//            default:
+//                System.out.print("-1 !");
+//        }
+//        System.out.print(" days");
+
+
+//        ZADANIE 7
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Give me number: ");
+//        int number = scanner.nextInt();
+//        while (number > 0) {
+//            System.out.print(number%10);
+//            number /= 10;
+//        }
+
+
+//        ZADANIE 8
+//
+//        int number;
+//        do {
+//            System.out.print("Give me number: ");
+//             number = scanner.nextInt();
+//        }while(number<1 || number>12);
+//        System.out.print("Your month has: ");
+//        switch(number){
+//            case 1:
+//                System.out.print(31);
+//                break;
+//            case 2:
+//                System.out.print(28);
+//                break;
+//            default:
+//                System.out.print("-1 !");
+//        }
+//        System.out.print(" days");
+
+
+
+//        ZADANIE 9
+//        Scanner scanner = new Scanner(System.in);
+//        int number;
+//        System.out.print("Give me number: ");
+//        number = scanner.nextInt();
+//        char star = '*';
+//        for(int i = 1; i<=number ; i++){
+//            for(int j = 0 ; j < i ; j++){
+//                System.out.print(star);
+//            }
+//            System.out.println();
+//        }
+
+//        ZADANIE 10
+//        int[] tab = {1,2,3,4,5};
+//        for(int number : tab){
+//            if(number%2==0){
+//                System.out.print(number);
+//            }
+//        }
+
+    }
+
+}
